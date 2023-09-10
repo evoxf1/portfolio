45464999027e0749ada521c466b91a442ca58f39
evoxf1 <sahil2812@live.com>
2023-09-10 13:27:42 +0530

    added section divider and intro component

---
+++ components/intro.tsx
@@ -0,0 +1,99 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+
+export default function Intro() {
+  return (
+    <section className=" relative mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="/sahil.png"
+              alt="Sahil Bhardwaj portrait"
+              width="300"
+              height="300"
+              quality="95"
+              priority={true}
+              className="h-[11rem] w-[11rem] rounded-full object-cover border-[0.35rem] border-gray shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute text-4xl bottom-0 right-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5]"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Sahil.</span> I'm a{" "}
+        <span className="font-bold">Full-Stack & Front-End Developer</span> with{" "}
+        <span>passion and expertise</span>. I thrive on creating{" "}
+        <span className="italic">captivating digital experiences</span>& my
+        forte is <span className="underline">React (Next.js) & MERN Stack</span>
+        .
+      </motion.p>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition cursor-pointer"
+        >
+          Drop Me a Line
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/SAHIL.pdf"
+          download
+        >
+          Grab My Resume
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/evoxf1/"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/evoxf1"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <BsGithub />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
